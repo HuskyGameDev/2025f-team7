fb7573ea023f4bfad1a67b45f65aec1e1226629a
EnderRoss30 <enderr@mtu.edu>
2025-10-09 19:07:18 -0400

    These are the files for the Sawblade movement

---
+++ saw.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+
+func _physics_process(delta: float) -> void:
+
+	var directionx := Input.get_axis("sawLeft", "sawRight")
+	var directiony :=Input.get_axis("sawUp", "sawDown")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	if directionx:
+		velocity.x = directionx * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
