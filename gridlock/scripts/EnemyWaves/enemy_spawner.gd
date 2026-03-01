fb4c9f2e78b42998e16e18d145e2e8c330221deb
gajrober <gajrober@mtu.edu>
2026-03-01 15:14:18 -0500

    Added a delay to EnemySpawners (and fixed some weird scaling behavior with them)

--- gridlock/scripts/EnemyWaves/enemy_spawner.gd
+++ gridlock/scripts/EnemyWaves/enemy_spawner.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 @export var spawns: PackedScene
-@export var spawn_time: float
+@export var spawn_time: float = 1.0
+@export var delay: float = 0.0
 
 signal spawned(node: Node2D)
 
-@onready var timer := 0.0
+@onready var timer := -delay
+@onready var start_scale: Vector2 = transform.get_scale()
 
-
+func _ready() -> void:
+	if timer < 0: visible = false
 
 func _process(delta: float) -> void:
 	timer += delta
@@ -15,12 +18,15 @@ func _process(delta: float) -> void:
 		__spawn()
 		return
 	
-	scale = Vector2.ONE * (1 - (timer / spawn_time))
+	if timer >= 0:
+		visible = true
+		scale = start_scale * (1 - (timer / spawn_time))
 
 func __spawn() -> void:
 	var node: Node2D = spawns.instantiate()
+	node.global_position = global_position
 	get_parent().add_child(node)
 	emit_signal("spawned", node)
 	
-	node.global_position = global_position
+	
 	queue_free()
