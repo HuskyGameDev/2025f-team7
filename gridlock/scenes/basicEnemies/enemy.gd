0a9fa10e6682f02d36e63ff7a11c22ce55b699d4
gajrober <gajrober@mtu.edu>
2026-03-14 15:08:45 -0400

    Updated the base enemy script to do a bit more
    - Now automatically detects the PlayerDetection node, which should make
      adding the enemy script to basic enemies a lot easier
    - Automatically tracks player position!
    - New signals for health change and death

--- gridlock/scenes/basicEnemies/enemy.gd
+++ gridlock/scenes/basicEnemies/enemy.gd
@@ -1,28 +1,64 @@
 class_name Enemy
-extends Area2D
+extends Node2D
 
-@export var MAX_HEALTH: float
+signal health_changed(new_health: float)
+signal died
 
-@onready var health := MAX_HEALTH:
+@export var max_health: float
+
+@onready var health := max_health:
 	get(): return health
 	set(value):
-		health = clampf(value, 0.0, MAX_HEALTH)
+		value = clampf(value, 0.0, max_health)
+		if health == value: return
+		
+		health = value
+		emit_signal("health_changed", health)
+
+var player_detection: Area2D:
+	get(): return player_detection
+	set(value):
+		print("setter triggered")
+		if player_detection == value: return
+		print("value is different")
+		
+		if player_detection:
+			print("disconnecting old one")
+			player_detection.body_entered.disconnect(_on_body_entered)
+			player_detection.body_exited.disconnect(_on_body_exited)
+		
+		player_detection = value
+		if player_detection:
+			print("connecting new one")
+			player_detection.body_entered.connect(_on_body_entered)
+			player_detection.body_exited.connect(_on_body_exited)
 
 @onready var taking_damage := false
 
-func _ready():
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
+var target: Vector2 = Vector2.ZERO
+
+func _init() -> void:
+	GlobalSignals.player_position.connect(_on_player_position)
+
+func _ready() -> void:
+	print("_ready")
+	player_detection = get_node("PlayerDetection")
+	assert(player_detection)
 
 func _process(delta: float) -> void:
 	if taking_damage:
 		health -= 10 * delta
 	
 	if health == 0.0:
+		emit_signal("died")
 		queue_free()
 
 func _on_body_entered(_body: PhysicsBody2D) -> void:
+	print("taking damage!!!")
 	taking_damage = true
 
 func _on_body_exited(_body: PhysicsBody2D) -> void:
 	taking_damage = false
+
+func _on_player_position(pos: Vector2) -> void:
+	target = pos
