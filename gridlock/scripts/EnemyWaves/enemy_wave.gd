1eb4ffa83ba86e2e952d2087f1a3b185930057da
gajrober <gajrober@mtu.edu>
2026-03-14 13:56:58 -0400

    Added a new export to enemy waves that lets you set an extra delay after all enemies die

--- gridlock/scripts/EnemyWaves/enemy_wave.gd
+++ gridlock/scripts/EnemyWaves/enemy_wave.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+# How long to wait after the wave is complete before
+# starting the next item on the Timeline
+@export var delay := 0.0
+
+@onready var timer := 0.0
+
 func _process(delta: float) -> void:
-	if get_child_count() == 0:
-		queue_free()
+	if get_child_count() != 0: return
+	timer += delta
+	if timer >= delay: queue_free()
