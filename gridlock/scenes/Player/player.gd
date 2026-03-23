fb903ac2389da26938fc64c7cee1ccb84898e7b1
gajrober <gajrober@mtu.edu>
2026-03-23 19:45:16 -0400

    Restore player's health when a boss dies
    Maybe something fancier in the future? But for now this works fine I think

--- gridlock/scenes/Player/player.gd
+++ gridlock/scenes/Player/player.gd
@@ -31,6 +31,7 @@ func _ready():
 	emit_signal("player_position", global_position)
 	invincible = false
 	GlobalSignals.bomb_gained.connect(_on_bomb_gained)
+	GlobalSignals.boss_died.connect(_on_boss_died)
 
 func _physics_process(_delta: float):
 	var currentSpeed = SPEED
@@ -109,3 +110,6 @@ func _on_bomb_gained() -> void:
 
 func _on_hit(_area: Area2D) -> void:
 	set_status(0)
+
+func _on_boss_died() -> void:
+	health = 10
